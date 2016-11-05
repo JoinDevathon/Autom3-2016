61be3c118ad9bde8754815cbdc937a825b00a8ac
Autom <tom.dewulf@ugent.be>
2016-11-05 21:05:00 +0100

    Thanks to Florian for reminding me that I should register my recipes, now it's a lot simpler

--- src/main/java/org/devathon/contest2016/listeners/CraftingListener.java
+++ src/main/java/org/devathon/contest2016/listeners/CraftingListener.java
@@ -1,21 +1,20 @@
 package org.devathon.contest2016.listeners;
 
+import gnu.trove.map.hash.THashMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType.SlotType;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.CraftingInventory;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.devathon.contest2016.recipe.CustomShapedRecipe;
 import org.devathon.contest2016.recipe.CustomShapelessRecipe;
 
@@ -27,32 +26,12 @@ public class CraftingListener implements Listener {
 
     private final List<CustomShapelessRecipe> shapelessRecipes = new ArrayList<>();
     private final List<CustomShapedRecipe> shapedRecipes = new ArrayList<>();
-    
+
     @EventHandler
     public void onPrepareItemCraft(PrepareItemCraftEvent event) {
-        for (ItemStack item : event.getInventory()) {
-            if (item == null || item.getItemMeta() == null) {
-                continue;
-            }
-            final List<String> lore = item.getItemMeta().getLore();
-            if (lore != null && !lore.isEmpty() && lore.get(0).equals(ChatColor.DARK_AQUA + "Devathon item")) {
-                event.getInventory().setResult(new ItemStack(Material.AIR));
-            }
-        }
-    }
-
-    @EventHandler
-    public void onCustomCraftPrepare(InventoryClickEvent event) {
-        if (event.getSlotType() != SlotType.CRAFTING) {
-            return;
-        }
-
         boolean customCraft = false;
 
-        ItemStack current = event.getCurrentItem();
-        event.setCurrentItem(event.getCursor());
-
-        for (ItemStack item : event.getClickedInventory()) {
+        for (ItemStack item : event.getInventory()) {
             if (item == null || item.getItemMeta() == null) {
                 continue;
             }
@@ -64,7 +43,6 @@ public class CraftingListener implements Listener {
         }
 
         if (!customCraft) {
-            event.setCurrentItem(current);
             return;
         }
 
@@ -72,7 +50,7 @@ public class CraftingListener implements Listener {
         for (CustomShapelessRecipe shapelessRecipe : shapelessRecipes) {
             final Map<ItemStack, Integer> amounts = shapelessRecipe.getIngredientAmounts();
 
-            for (ItemStack item : event.getClickedInventory()) {
+            for (ItemStack item : event.getInventory()) {
                 Integer amount = amounts.get(item);
                 if (amount == null) {
                     continue;
@@ -86,47 +64,66 @@ public class CraftingListener implements Listener {
                     continue outer;
                 }
             }
-
-            //This is supposed to work, but apparently it doesn't :(
-            //((CraftingInventory) event.getClickedInventory()).setResult(shapelessRecipe.getResult());
-            //I guess I'll have to use a different solution
-            event.setCurrentItem(current);
-            craftItem(event.getWhoClicked(), event.getClickedInventory(), shapelessRecipe.getResult());
             return;
         }
 
         outer:
         for (CustomShapedRecipe shapedRecipe : shapedRecipes) {
-            for (int i = 0; i < ((CraftingInventory) event.getClickedInventory()).getMatrix().length; i++) {
+            for (int i = 0; i < ((CraftingInventory) event.getInventory()).getMatrix().length; i++) {
                 if (shapedRecipe.getRecipe()[i] == null) {
                     continue;
                 }
-                final ItemStack item = ((CraftingInventory) event.getClickedInventory()).getMatrix()[i];
+                final ItemStack item = ((CraftingInventory) event.getInventory()).getMatrix()[i];
                 if (item == null || !item.equals(shapedRecipe.getRecipe()[i])) {
                     continue outer;
                 }
             }
-
-            event.setCurrentItem(current);
-            craftItem(event.getWhoClicked(), event.getClickedInventory(), shapedRecipe.getResult());
             return;
         }
 
-        event.setCurrentItem(current);
-    }
-
-    private void craftItem(HumanEntity player, Inventory inventory, ItemStack result) {
-        inventory.clear();
-        player.setItemOnCursor(null);
-        player.getWorld().dropItemNaturally(player.getLocation(), result);
-        player.closeInventory();
+        event.getInventory().setResult(new ItemStack(Material.AIR));
     }
 
     public void registerCraftingRecipe(Recipe recipe) {
         if (recipe instanceof CustomShapelessRecipe) {
-            shapelessRecipes.add((CustomShapelessRecipe) recipe);
+            final CustomShapelessRecipe customShapelessRecipe = (CustomShapelessRecipe) recipe;
+            final ShapelessRecipe shapelessRecipe = new ShapelessRecipe(recipe.getResult());
+
+            for (ItemStack ingredient : customShapelessRecipe.getIngredients()) {
+                shapelessRecipe.addIngredient(ingredient.getType(), ingredient.getDurability());
+            }
+
+            shapelessRecipes.add(customShapelessRecipe);
+            Bukkit.getServer().addRecipe(shapelessRecipe);
         } else if (recipe instanceof CustomShapedRecipe) {
-            shapedRecipes.add((CustomShapedRecipe) recipe);
+            final CustomShapedRecipe customShapedRecipe = (CustomShapedRecipe) recipe;
+            final ShapedRecipe shapedRecipe = new ShapedRecipe(recipe.getResult());
+
+            int i = 0;
+            char character = 'a';
+            String[] shape = new String[3];
+            Map<ItemStack, Character> items = new THashMap<>();
+            for (ItemStack ingredient : customShapedRecipe.getRecipe()) {
+                final ItemStack itemStack = new ItemStack(ingredient.getType(), 1, ingredient.getDurability());
+                Character c = items.get(itemStack);
+                if (c == null) {
+                    c = character;
+                    character++;
+                    items.put(itemStack, c);
+                }
+                if (i % 3 == 0) {
+                    shape[i / 3] = "";
+                }
+                shape[i / 3] += c;
+                i++;
+            }
+            shapedRecipe.shape(shape);
+            for (Map.Entry<ItemStack, Character> entry : items.entrySet()) {
+                shapedRecipe.setIngredient(entry.getValue(), entry.getKey().getType(), entry.getKey().getDurability());
+            }
+
+            shapedRecipes.add(customShapedRecipe);
+            Bukkit.getServer().addRecipe(shapedRecipe);
         } else {
             Bukkit.getServer().addRecipe(recipe);
         }
