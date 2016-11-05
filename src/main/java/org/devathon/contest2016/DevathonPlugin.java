77a4e00cccfe96b4806cdabbd9a3fc8dd1328d6b
Autom <tom.dewulf@ugent.be>
2016-11-05 14:34:38 +0100

    Lets get this started!

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -6,12 +6,12 @@ public class DevathonPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // put your enable code here
+        getLogger().info("Woo! Machines activate!");
     }
 
     @Override
     public void onDisable() {
-        // put your disable code here
+        getLogger().info("Beep boop. Shutting down...");
     }
 }
 
